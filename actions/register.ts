faad8131b0f3a477dfc9583ec68d42f6fbd92d83
GetoarNishefci <getoar.n1@gmail.com>
2025-10-15 12:19:26 +0200

    feat:create a user

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server"
 
 import * as z from "zod"
+import bcrypt from "bcrypt"
 
 import { RegisterSchema } from "@/schemas";
+import db from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) =>{
 
@@ -12,5 +15,23 @@ export const register = async (values:z.infer<typeof RegisterSchema>) =>{
         return {error:"Invalid fields!"}
     }
 
-    return { success: "Email sent!"}
+    const { email, password,name } = validateFields.data
+
+    const hashedPassowrnd = await bcrypt.hash(password,10)
+
+ const existingUser = await getUserByEmail(email);
+
+if(existingUser){
+    return { error: "Enail already in use!"}
+}
+
+await db.user.create({
+    data:{
+        name,
+        email,
+        password:hashedPassowrnd,
+    }
+})
+
+    return { success: "User created!"}
 };
