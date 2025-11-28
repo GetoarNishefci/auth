8ef8e4eb980720f7ad476fb888ea32381a61ad65
GetoarNishefci <getoar.n1@gmail.com>
2025-11-28 12:12:49 +0100

    fix: deploy error

--- middleware.ts
+++ middleware.ts
@@ -2,37 +2,36 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
 
-const {auth}= NextAuth(authConfig);
+const {auth} = NextAuth(authConfig);
 
 export default auth((req) => {
     const {nextUrl} = req;
-    const isLoggedIn = !!req.auth
+    const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoure = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
     const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-
-        if(isApiAuthRoure){
-            return null
+    if(isApiAuthRoute){
+        return null;
+    }
+    
+    if(isAuthRoute){
+        if(isLoggedIn){
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl));
         }
-        
-        if(isAuthRoute){
-                if(isLoggedIn){
-                    return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT,nextUrl))
-                }
-            return null
-        }
-        
-        if(!isLoggedIn && !isPublicRoute){
-            return Response.redirect(new URL("/auth/login",nextUrl))
-        }
+        return null;
+    }
+    
+    if(!isLoggedIn && !isPublicRoute){
+        return Response.redirect(new URL("/auth/login",nextUrl));
+    }
 
-        return null
-
+    return null;
 });
 
 export const config = {
+  runtime: 'nodejs', 
   matcher: [
     '/((?!.*\\..*|_next).*)',
     '/',
