db578e5e649e734e49979bca73edeb4cd73bbf2c
GetoarNishefci <getoar.n1@gmail.com>
2025-11-29 12:18:06 +0100

    fix:deploy error

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,38 +1,37 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { ProductService } from "@/services/product.service";
 
 export async function GET(
-  _: Request,
-  { params }: { params: { id: string } }
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const product = await ProductService.getById(params.id);
+  const { id } = await params;
+  const product = await ProductService.getById(id);
 
   if (!product) {
-    return NextResponse.json(
-      { message: "Product not found" },
-      { status: 404 }
-    );
+    return NextResponse.json({ message: "Product not found" }, { status: 404 });
   }
 
   return NextResponse.json(product);
 }
 
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await params;
   const body = await req.json();
-
-  const updated = await ProductService.update(params.id, body);
+  const updated = await ProductService.update(id, body);
 
   return NextResponse.json(updated);
 }
 
 export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
+  _: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  await ProductService.delete(params.id);
+  const { id } = await params; 
+  await ProductService.delete(id);
 
   return NextResponse.json({ message: "Deleted successfully" });
 }
